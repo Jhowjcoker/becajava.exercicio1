a7b5142153eaac127acf1c2b45dfd7a093718eab
Jonathan Rodrigues Santos <jrsantos@everis.com>
2020-10-25 23:16:59 -0300

    first commit

--- src/exercicio1/Exercicio1.java
+++ src/exercicio1/Exercicio1.java
@@ -1,12 +1,19 @@
 package exercicio1;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 
 	public static void main(String[] args) {
-		System.out.println("E ae mundo doido");
 		
+		System.out.println("Digite a quantidade de meses a serem calculados");
 		
-
+		Scanner entrada = new Scanner(System.in);
+		int meses = entrada.nextInt();
+		
+		int resultado = meses * 30;
+		
+		System.out.println("A quantidade de meses calculados é: " + resultado + "dias!");
 	}
 
 }
